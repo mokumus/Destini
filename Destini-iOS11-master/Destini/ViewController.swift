b594115295e9ac8c86b607f48a48b78bfe17f671
Flamong Butterscotch <rocketpolka@gmail.com>
2018-02-26 03:35:31 +0300

    Add files via upload

---
+++ Destini-iOS11-master/Destini/ViewController.swift
@@ -0,0 +1,122 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Philipp Muellauer on 01/09/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Our strings
+    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
+    let answer1a = "I\'ll hop in. Thanks for the help!"
+    let answer1b = "Better ask him if he\'s a murderer first."
+    
+    
+    let story2 = "He nods slowly, unphased by the question."
+    let answer2a = "At least he\'s honest. I\'ll climb in."
+    let answer2b = "Wait, I know how to change a tire."
+    
+    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
+    let answer3a = "I love Elton John! Hand him the cassette tape."
+    let answer3b = "It\'s him or me! You take the knife and stab him."
+    
+    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
+    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
+    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
+    
+    
+    // UI Elements linked to the storyboard
+    @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
+    @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
+    @IBOutlet weak var storyTextView: UILabel!	
+	@IBOutlet weak var restartButton: UIButton!
+	
+	
+    // TODO Step5: Initialise instance variables here
+	var currentStage : Int = 1
+	var currentChoice : Int = 0
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+		setStage()
+		
+    }
+
+    
+    // User presses one of the buttons
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    
+		
+		currentChoice = sender.tag
+		updateUI(stage: currentStage,choice: currentChoice)
+		
+		
+		if currentStage == 4 || currentStage == 5 || currentStage == 6{
+			restartButton.isHidden = false
+		}
+    
+    }
+    
+	func updateUI(stage : Int, choice : Int){
+		
+		if currentStage == 1 || currentStage == 2{ // START OF GAME
+			if choice == 1{
+				topButton.setTitle(answer3a, for: .normal)
+				bottomButton.setTitle(answer3b, for: .normal)
+				storyTextView.text = story3
+				currentStage = 3
+			}
+			else if choice == 2 && currentStage == 1{
+				topButton.setTitle(answer2a, for: .normal)
+				bottomButton.setTitle(answer2b, for: .normal)
+				storyTextView.text = story2
+				currentStage = 2
+			}
+		}
+		else if currentStage == 3{ //ENDING 6
+			if choice == 1{
+				topButton.isHidden = true
+				bottomButton.isHidden = true
+				storyTextView.text = story6
+				currentStage = 6
+			}
+			else if choice == 2{ // ENDING 5
+				topButton.isHidden = true
+				bottomButton.isHidden = true
+				storyTextView.text = story5
+				currentStage = 5
+			}
+		}
+		else if currentStage == 2{ // ENDING 4
+			topButton.isHidden = true
+			bottomButton.isHidden = true
+			storyTextView.text = story4
+			currentStage = 4
+		}
+	}
+	
+	func setStage(){ // Start of the story
+		topButton.isHidden = false
+		bottomButton.isHidden = false
+		restartButton.isHidden = true
+		
+		topButton.setTitle(answer1a, for: .normal)
+		bottomButton.setTitle(answer1b, for: .normal)
+		
+		storyTextView.text = story1
+		
+		currentStage = 1
+		
+	}
+	@IBAction func restart(_ sender: UIButton) {
+		setStage()
+	}
+}
+
+
